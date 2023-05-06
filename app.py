7846c91eaa00d10359d397e76669ddaaa1acb7ce
Bellazulia <Bellazulia>
2023-05-06 11:52:36 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,55 +1,42 @@
-from flask import Flask, render_template, jsonify, request
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-from datetime import datetime
 
-connection_string = 'mongodb+srv://bella:bellasari07@cluster0.31i9km7.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(connection_string)
-db = client.dbBella
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-
-@app.route('/diary', methods=['GET'])
-def show_diary():
-    articles = list(db.diary.find({}, {'_id': False}))
-    return jsonify({'articles': articles})
-
-
-@app.route('/diary', methods=['POST'])
-def save_diary():
-    # sample[_receive = request.form['sample_give']
-    # printsample_receive]
-    title_receive = request.form.get("title_give")
-    content_receive = request.form.get("content_give")
-
-    today = datetime.now()
-    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
-
-    file = request.files['file_give']
-    extension = file.filename.split('.')[-1]
-    filename = f'file-{mytime}.{extension}'
-    save_to = f'static/{filename}'
-    file.save(save_to)
-
-    profile = request.files['profile_give']
-    extension = profile.filename.split('.')[-1]
-    profilename = f'profile-{mytime}.{extension}'
-    save = f'static/{profilename}'
-    profile.save(save)
-
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
     doc = {
-        'file' : filename,    
-        'profile' : profilename,
-        'title': title_receive,
-        'content': content_receive,
+        'name': name_receive,
+        'comment': comment_receive,
     }
-    db.diary.insert_one(doc)
-    return jsonify({'msg': 'POST request complete!'})
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'Comment Posted!'})
 
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
-if __name__  == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
